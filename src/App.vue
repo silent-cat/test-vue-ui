474109421e8a6a547e992f6387b74b0bab5d80bb
qizhixin <13284167983@163.com>
2021-01-27 14:01:08 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Button theme="link">1123</Button>
+  <Switch v-model:value="value"></Switch>
+  <Layout>
+    <Header>header</Header>
+    <Content>content</Content>
+    <Footer>footer</Footer>
+  </Layout>
+  <Input v-model:value="value1" placeholder="请输入内容" />
+  <Input :value="value1" />
+
+  <Tabs v-model:selected="x">
+    <Tab title="导航一">内容一</Tab>
+    <Tab title="导航二">内容二</Tab>
+    <Tab title="导航三">内容三。。。。。。。。。。。。</Tab>
+  </Tabs>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+<script lang="ts">
+import {
+  Switch,
+  Button,
+  Tabs,
+  Tab,
+  Dialog,
+  Input,
+  Layout,
+  Header,
+  Content,
+  Footer,
+  Aside,
+} from "language-ui-1";
+import "language-ui-1/dist/lib/language.css";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      value: false,
+      value1: null,
+      x: "导航二",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Switch,
+    Button,
+    Tabs,
+    Tab,
+    Dialog,
+    Input,
+    Layout,
+    Header,
+    Content,
+    Footer,
+    Aside,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
